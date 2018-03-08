c0e1a62e1fc55d859e66f2c8cceb55eea7bfa2e3
Philippe Marschall <philippe.marschall@gmail.com>
2018-03-08 21:24:57 +0100

    start with cpuid

--- src/main/c/com_github_marschall_rdrand_Rdrand.c
+++ src/main/c/com_github_marschall_rdrand_Rdrand.c
@@ -26,5 +26,21 @@ JNIEXPORT jboolean JNICALL Java_com_github_marschall_rdrand_Rdrand_isAvailable0
   (JNIEnv *env, jclass clazz)
 {
 
-  return 0;
+  unsigned int eax, ebx, ecx, edx;
+  if (__get_cpuid(1, &eax, &ebx, &ecx, &edx))
+  {
+    /* if (((ecx & bit_RDRND) == bit_RDRND) && ((ebx & bit_RDSEED) == bit_RDSEED)) */
+    if (((ecx & bit_RDRND) == bit_RDRND))
+    {
+      return JNI_TRUE;
+    }
+    else
+    {
+      return JNI_FALSE;
+    }
+  }
+  else
+  {
+    return JNI_FALSE;
+  }
 }
