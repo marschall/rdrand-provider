70354a498c10b029c82566a202adb3d479172de3
Philippe Marschall <philippe.marschall@gmail.com>
2018-03-08 21:34:47 +0100

    better checks

--- src/main/c/com_github_marschall_rdrand_Rdrand.c
+++ src/main/c/com_github_marschall_rdrand_Rdrand.c
@@ -3,6 +3,10 @@
 #include <immintrin.h>
 #include <cpuid.h>
 
+#ifndef bit_RDSEED
+  #define bit_RDSEED  (1 << 18)
+#endif
+
 #include "com_github_marschall_rdrand_Rdrand.h"
 
 
@@ -26,11 +30,12 @@ JNIEXPORT jboolean JNICALL Java_com_github_marschall_rdrand_Rdrand_isAvailable0
   (JNIEnv *env, jclass clazz)
 {
 
-  unsigned int eax, ebx, ecx, edx;
-  if (__get_cpuid(1, &eax, &ebx, &ecx, &edx))
+  unsigned int sig, eax, ebx, ecx, edx;
+
+  __get_cpuid_max (0x80000000, &sig);
+  if (__get_cpuid(sig, &eax, &ebx, &ecx, &edx))
   {
-    /* if (((ecx & bit_RDRND) == bit_RDRND) && ((ebx & bit_RDSEED) == bit_RDSEED)) */
-    if (((ecx & bit_RDRND) == bit_RDRND))
+    if (((ecx & bit_RDRND) == bit_RDRND) && ((ebx & bit_RDSEED) == bit_RDSEED))
     {
       return JNI_TRUE;
     }
