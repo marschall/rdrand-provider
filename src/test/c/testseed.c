7a0f2940bdfd1311c9926743d20af42b740178fe
Philippe Marschall <philippe.marschall@gmail.com>
2018-03-09 10:51:25 +0100

    proper cpuid check

--- src/test/c/testseed.c
+++ src/test/c/testseed.c
@@ -9,11 +9,20 @@ int main(void)
 {
   unsigned int sig, eax, ebx, ecx, edx;
 
-  __get_cpuid_max(0x80000000, &sig);
+  __get_cpuid_max(0, &sig);
   printf("sig %d\n", sig);
-  if (__get_cpuid(sig, &eax, &ebx, &ecx, &edx))
+  if (__get_cpuid(7, &eax, &ebx, &ecx, &edx))
   {
     printf("(ebx & bit_RDSEED) == bit_RDSEED %d\n", (ebx & bit_RDSEED) == bit_RDSEED);
+    printf("true\n");
+  }
+  else
+  {
+    printf("false\n");
+  }
+  if (__get_cpuid(1, &eax, &ebx, &ecx, &edx))
+  {
+    printf("(ecx & bit_RDRND) == bit_RDRND %d\n", (ecx & bit_RDRND) == bit_RDRND);
     printf("true\n");
   }
   else
