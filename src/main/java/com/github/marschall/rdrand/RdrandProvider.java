4ec36ec3ff4fb9124f5234cfa5d3dabdf6b0f9f8
Philippe Marschall <philippe.marschall@gmail.com>
2018-03-09 11:18:49 +0100

    fix javadoc

--- src/main/java/com/github/marschall/rdrand/RdrandProvider.java
+++ src/main/java/com/github/marschall/rdrand/RdrandProvider.java
@@ -4,10 +4,10 @@ import java.security.Provider;
 
 /**
  * A security provider that installs one random number generation
- * algorithms that use the <a href="https://lwn.net/Articles/606141/">RDAND</a>
- * and <code>RDSEED<code> hardware instructions.
+ * algorithms that use the <a href="https://en.wikipedia.org/wiki/RdRand">RDAND</a>
+ * and <code>RDSEED</code> hardware instructions.
  *
- * @see <a href="https://docs.oracle.com/javase/8/docs/technotes/guides/security/crypto/HowToImplAProvider.html">How to Implement a Provider in the Java Cryptography Architecture</a>
+ * @see <a href="https://docs.oracle.com/javase/9/security/howtoimplaprovider.htm#JSSEC-GUID-C485394F-08C9-4D35-A245-1B82CDDBC031">How to Implement a Provider in the Java Cryptography Architecture</a>
  */
 public final class RdrandProvider extends Provider {
 
@@ -23,6 +23,10 @@ public final class RdrandProvider extends Provider {
 
   private static final long serialVersionUID = 1L;
 
+  /**
+   * Default constructor, either called directly by programmatic registration or
+   * by JCA.
+   */
   public RdrandProvider() {
     super(NAME, 0.1d, "rdrand (SecureRandom)");
     this.put("SecureRandom." + ALGORITHM, RdrandSecureRandomSpi.class.getName());
