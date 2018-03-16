ecefd7ec3be1f8f4170f620cba75f30c9aad196b
Philippe Marschall <philippe.marschall@gmail.com>
2018-03-16 14:39:59 +0100

    - assert alignement

--- src/test/java/com/github/marschall/rdrand/AlignedAccessTest.java
+++ src/test/java/com/github/marschall/rdrand/AlignedAccessTest.java
@@ -1,7 +1,10 @@
 package com.github.marschall.rdrand;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.FieldLayout;
 
 class AlignedAccessTest {
 
@@ -9,6 +12,13 @@ class AlignedAccessTest {
   void byteArrayDoubleWordAligned() {
     ClassLayout classLayout = ClassLayout.parseInstance(new byte[2]);
     System.out.println(classLayout.toPrintable());
+
+    FieldLayout byteField = classLayout.fields().stream()
+            .filter(fieldLayout -> fieldLayout.typeClass().equals("byte"))
+            .findFirst()
+            .get();
+
+    assertEquals(0, byteField.offset() % 8, "byte array content is not double word aligned");
   }
 
 }
